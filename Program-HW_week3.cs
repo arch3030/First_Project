c223c650528bfacd239dba7608c2094099a8c1c0
arch3030 <78534135+arch3030@users.noreply.github.com>
2021-03-09 01:12:53 +0700

    Add files via upload

---
+++ Program-HW_week3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Parabola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INFO
+            double X, Y, A, B;                                   //สมการ :  Y=(X-1)2 or Math.Sqrt(Y)+1 
+            string Mode;                                         //A และ B แทน X
+            Console.Write("Input X :");
+            X = double.Parse(Console.ReadLine());
+            Console.Write("Input Y:");
+            Y = double.Parse(Console.ReadLine());
+            Console.Write("Input Mode :");
+            Mode = Console.ReadLine();
+            if (Mode == "time")
+            {
+                if(X < 0)
+                {
+                    Console.WriteLine("Invalid Mouse Position");
+                }
+                else if(X >= 0)
+                {
+                    Y = Math.Pow(X-1,2);
+                    Console.WriteLine("Output : {0} , {1}", X, Y);
+                }
+            }
+            else if(Mode == "price")
+            {
+                if(X < 0)
+                {
+                    Console.WriteLine("Invalid Mouse Position");
+                }
+                else if( X >= 0)
+                {
+                    A = Math.Sqrt(Y) + 1;
+                    Console.WriteLine("Output : {0} , {1}", A, Y);
+                }
+                //else if((X <= 1)&&(Y <= 1))                         //โจทย์ไม่ได้กำหนดแต่เพื่อให้ครบกรณี
+                //{
+                //    B = -Math.Sqrt(Y) + 1;
+                //    Console.WriteLine("Output : {0} , {1}", B, Y);
+                //}
+            }
+            else
+            {
+                Console.WriteLine("Invalid Mode");
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
